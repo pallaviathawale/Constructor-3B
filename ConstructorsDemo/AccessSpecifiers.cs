cbc7ce88c94eadb2d8c7c5224ef1b8879c14badf
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-15 16:57:45 +0530

    [Pallavi] Add: AccessSpecifiers

---
+++ ConstructorsDemo/AccessSpecifiers.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsDemo
+{
+    //internal Access Specifier
+    internal class InternalModifiers
+    {
+        int Num1;
+        int Num2;
+
+        public void SetData(int a, int b)
+        {
+            Num1 = a;
+            Num2 = b;
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine("Number 1 is: " +Num1);
+            Console.WriteLine("Number 2 is: " + Num2);
+        }
+    }
+     //Private Acees Specifier
+    public class BankAccount
+    {
+        private decimal balance;
+
+        public BankAccount(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Funds..");
+            }
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+    //Protected Access Specifier
+    public class Base
+    {
+        protected int num1;
+    }
+
+    public class Derived : Base
+    {
+        public int num2;
+
+        public int Add()
+        {
+            num1 = num2;
+
+            return num1 + num2;
+
+        }
+    }
+    //Public Aceess Specifier
+    public class Student
+    {
+        public string name = "Rahul";
+
+        public void print()
+        {
+            Console.WriteLine("Hello there");
+        }
+    }
+}
--- ConstructorsDemo/Program.cs
+++ ConstructorsDemo/Program.cs
@@ -13,13 +13,53 @@ namespace ConstructorsDemo
             //Constructor obj1 = new Constructor();
             //obj1.Display();
 
-            IDrawable drawable;
-            drawable = new Addition();
-            drawable.Operation();
-            drawable = new Subtraction();
-            drawable.Operation();
-            drawable = new Multiplication();
-            drawable.Operation();
+            //IDrawable drawable;
+            //drawable = new Addition();
+            //drawable.Operation();
+            //drawable = new Subtraction();
+            //drawable.Operation();
+            //drawable = new Multiplication();
+            //drawable.Operation();
+
+            //AccessSpecifiers accessSpecifiers = new AccessSpecifiers();
+            //Console.WriteLine(accessSpecifiers.PublicField);
+            // Console.WriteLine(accessSpecifiers.PrivateField); // Error: PrivateField is inaccessible
+            // Console.WriteLine(accessSpecifiers.ProtectedField); // Error: ProtectedField is inaccessible
+           
+            Console.WriteLine(".....Access Modifiers....\n");
+
+            //private Access modifier
+
+            BankAccount obj = new BankAccount(1000);
+
+            obj.Deposit(500);
+            decimal balance1 = obj.GetBalance();
+            Console.WriteLine("\nAfter deposit balance is: " + balance1);
+            obj.Withdraw(100);
+            decimal balance2 = obj.GetBalance();
+            Console.WriteLine("After withdraw balance is: " + balance2);
+            //Public Access modifier
+            Student student = new Student();
+
+            Console.WriteLine("Name: " + student.name);
+
+            student.print();
+
+            //Protected Acess modifier
+
+            Derived derived = new Derived();
+
+            derived.num2 = 100;
+            int result = derived.Add();
+            Console.WriteLine("\nSum of two nums is :" + result + "\n");
+
+            //internal Access modifier
+
+            InternalModifiers modifiers = new InternalModifiers();
+
+            modifiers.SetData(22, 32);
+            modifiers.DisplayData();
+
         }
     }
 }
