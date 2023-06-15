9415da0629c3d5d70d02c058260d29d29fd25ca1
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-15 16:31:14 +0530

    [PALLAVI] Add: Interfaces

---
+++ ConstructorsDemo/InterfaceDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsDemo
+{
+    internal class InterfaceDemo
+    {
+
+        public interface IDrawable
+        {
+            void Operation();
+
+        }
+
+        public class Addition : IDrawable
+        {
+            public void Operation()
+            {
+                Console.WriteLine(".....Perform Addition.....");
+            }
+        }
+
+        public class Subtraction : IDrawable
+        {
+            public void Operation()
+            {
+                Console.WriteLine(".....Perform Subtraction.....");
+            }
+        }
+
+        public class Multiplication : IDrawable
+        {
+            public void Operation()
+            {
+                Console.WriteLine(".....Perform Multiplication....");
+            }
+        }
+
+    }
+}
+
--- ConstructorsDemo/Program.cs
+++ ConstructorsDemo/Program.cs
@@ -1,15 +1,25 @@
-﻿namespace ConstructorsDemo
+﻿using static ConstructorsDemo.InterfaceDemo;
+
+namespace ConstructorsDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            Constructor obj = new Constructor("pallavi", 25);
-            obj.Display();
+            //Constructor obj = new Constructor("pallavi", 25);
+            //obj.Display();
 
-            Constructor obj1 = new Constructor();
-            obj1.Display();
+            //Constructor obj1 = new Constructor();
+            //obj1.Display();
+
+            IDrawable drawable;
+            drawable = new Addition();
+            drawable.Operation();
+            drawable = new Subtraction();
+            drawable.Operation();
+            drawable = new Multiplication();
+            drawable.Operation();
         }
     }
 }
