6ad8757aa7136480616465d6fad394fbfd1d5bba
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-15 16:19:18 +0530

    [Pallavi] Add: File

---
+++ ConstructorsDemo/Constructor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsDemo
+{
+    internal class Constructor
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //   // Default Constructor
+        public Constructor()
+        {
+            Name = "Gnaga";
+            Age = 25;
+        }
+    
+
+
+    // Parameterized constructor
+    public Constructor(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+
+        }
+    }
+    
+}
---
+++ ConstructorsDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConstructorsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Constructor obj = new Constructor("pallavi", 25);
+            obj.Display();
+
+            Constructor obj1 = new Constructor();
+            obj1.Display();
+        }
+    }
+}
